55bda0f54b3eadf47f2f16276f22b39cfecc0c1b
果子树 <554638047@qq.com>
2021-12-02 16:29:43 +0800

    edit app.js

--- uploadFormFile/app.js
+++ uploadFormFile/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fileUpload = require('express-fileupload');
 const fs = require("fs")
 const path = require("path")
 const util = require('util');
@@ -9,13 +10,29 @@ app.use(express.static('.'))
 app.use(express.json({
   limit: 50 * 1024 * 1024
 }));
+app.use(fileUpload({
+  limit: 50 * 1024 * 1024
+}));
 
-app.post("/uploadBase64File", async (req, res) => {
-  const {file, fileName, fileType} = req.body;
-  const originalFile = file.replace(/^data:image\/\w+;base64,/, "");
-  const writeFile = util.promisify(fs.writeFile);
-  const result = await writeFile(`${__dirname}/files/${fileName}`, originalFile, { encoding: 'base64'})
-  res.send(result)
+app.post("/uploadFormFile", (req, res) => {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/files/' + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send({
+      result: 'File uploaded!'
+    });
+  });
 })
 
 app.get('/', (req, res) => {
